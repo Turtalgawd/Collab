212a5ab0d9ade8e933543cf8d851ab127a90e5b9
Turtalgawd <118429924+Turtalgawd@users.noreply.github.com>
2024-03-09 16:47:06 +0800

    fwfwfwe

--- battleship_server_OngJunWei_24S02B.py
+++ battleship_server_OngJunWei_24S02B.py
@@ -6,69 +6,56 @@ my_socket.listen()
 print('Waiting connection.....')
 
 targets = []
+final = []
+shots = 7
 y = -1
 with open('GAME.txt','r') as f:
     for line in f:
         y += 1
+        line1 = list(line)
+        final.append(line1)
         for x, char in enumerate(line):
             if char == 'S':
                 targets.append((x,y))
 f.close()
 board = [['.']*12 for _ in range(8)]
 
-array = ''
-for row in board:
-    row = ' '.join(row)
-    array += row + '\n'
-
 game_socket,addr = my_socket.accept()
 print('Connected to client')
 
-hits = []
-attempts = 7
-for i in range(7):
-    if i == 0:
-        game_socket.send(b'Game start\n' + array.encode())
-    print('Waiting for coords...')
-
-    rec = game_socket.recv(1024).decode()
-    print(rec)
-    status = ''
-    xcoord, ycoord = rec.split(',')
-    xcoord = int(xcoord)
-    ycoord = int(ycoord)
-    shoot = (xcoord,ycoord)
-    if shoot in targets:
-        hits.append(shoot)
-        board[ycoord][xcoord] = 'X'
-        status = 'Hit'
-        print(status)
-    else:
-        board[ycoord][xcoord] = 'O'
-        status = 'Miss'
-        print(status)
-    
+while True:
     array = ''
     for row in board:
         row = ' '.join(row)
         array += row + '\n'
+    array_final = ''
+    for row in final:
+        row = ' '.join(row)
+        array_final += row 
+
+    if shots == 7:
+        game_socket.send(b'Game start\n' + array.encode())
+    elif shots == 0:
+        game_socket.send(array_final.encode() + b'Game over\n')
+        break
+    else:
+        game_socket.send(array.encode())
     
-    game_socket.send(array.encode() + status.encode())
-
-y=-1
-with open('GAME.txt','r') as f:
-    for line in f:
-        y += 1
-        for x, char in enumerate(line):
-            if char == 'S':
-                if board[y][x] == '.':
-                    board[y][x] = 'S'
-f.close()
-array = ''
-for row in board:
-    row = ' '.join(row)
-    array += row + '\n'
-game_socket.send(array.encode() + b'Game over\n')
-
+    print('Waiting for coords...')
+    xcoord, ycoord = game_socket.recv(1024).decode().split(',')
+    xcoord = int(xcoord)
+    ycoord = int(ycoord)
+    loc = (xcoord,ycoord)
+    if loc in targets:
+        board[ycoord][xcoord] = 'X'
+        final[ycoord][xcoord] = 'X'
+        print(loc)
+        print('Hit')
+    else:
+        board[ycoord][xcoord] = 'O'
+        final[ycoord][xcoord] = 'O'
+        print(loc)
+        print('Miss')
+    shots -= 1
 my_socket.close()
 game_socket.close()
