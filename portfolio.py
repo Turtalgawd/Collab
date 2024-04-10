31d9d742059eba9f03d36d985173296e43a1cc1e
Turtalgawd <118429924+Turtalgawd@users.noreply.github.com>
2024-04-10 12:26:00 +0800

    GF2W
    G

--- portfolio.py
+++ portfolio.py
@@ -6,6 +6,9 @@ app = flask.Flask(__name__)
 def homepage():
     return flask.render_template('temphtmlpage.html')
 
+@app.route('/info')
+def info():
+    return flask.render_template('bryan_portfolio.html')
 
 
 app.run(debug=True)
