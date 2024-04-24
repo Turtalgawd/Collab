5774adc0d39f7b20e4428928a41092e7133453b5
Bryan Teo <71330264+Brynewater@users.noreply.github.com>
2024-04-24 09:50:35 +0800

    bread

--- portfolio.py
+++ portfolio.py
@@ -1,5 +1,6 @@
 import flask
 
+
 app = flask.Flask(__name__)
 
 @app.route('/')
@@ -9,6 +10,14 @@ def homepage():
 @app.route('/<name>')
 def info(name):
     return flask.render_template(f'{name}_portfolio.html')
+
+@app.route('/clicker')
+def clicker():
+    return flask.render_template('/bryan_projects/clicker/clicker.html')
+
+@app.route('/bread')
+def bread():
+    return flask.render_template('/bryan_projects/breadland/breadland.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
